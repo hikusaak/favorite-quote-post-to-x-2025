0071a53ad1b287974441ef9f410884fddb55b6f7
Osamu Takiya <takiya@toran.sakura.ne.jp>
2025-09-28 17:37:38 +0900

    fix: 🐛 crossorigin -> crossOrigin=""

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -23,7 +23,7 @@ export const renderer = jsxRenderer(({ children }, c) => {
         <link rel="icon" href="/favicon-192x192.png" sizes="192x192" />
         <link rel="apple-touch-icon" href="/favicon-180x180.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap"
