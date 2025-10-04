4e0e587249860792e7f549f57ed2fa328b8cbfe3
Osamu Takiya <takiya@toran.sakura.ne.jp>
2025-10-04 22:41:53 +0900

    fix: 🐛 半角スペースが + になってしまう現象を修正した

--- src/lib/stringOperation.ts
+++ src/lib/stringOperation.ts
@@ -2,13 +2,15 @@ const parseFormData = (postData: string): { [key: string]: string } => {
   // NOTE: デバッグ用なので production では消していい
   console.debug('postData:', postData)
 
-  const decodedData = decodeURIComponent(postData)
-  const params = decodedData.split('&')
+  const params = postData.split('&')
   const parsedData: { [key: string]: string } = {}
 
   params.forEach((param) => {
     const [key, value] = param.split('=')
-    parsedData[key] = value
+    if (key && value !== undefined) {
+      // + を半角スペースに変換してから decodeURIComponent でデコード
+      parsedData[key] = decodeURIComponent(value.replace(/\+/g, ' '))
+    }
   })
 
   // NOTE: デバッグ用なので production では消していい
